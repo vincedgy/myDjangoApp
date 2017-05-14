24566fc8551d3b7d789368aaa2fb2fed76f02048
Vincent <vincent.dagoury@gmail.com>
2017-05-14 11:00:18 +0200

    Updating views and add UnitTest

--- WebTest.py
+++ WebTest.py
@@ -1,6 +1,16 @@
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Chrome()
+driver = webdriver.Chrome('./chromedriver')
 
-browser.get('http://localhost:8000/polls/')
 
+class WebTest(unittest.TestCase):
+    def testPollsHomePage(self):
+        driver.get('http://localhost:8000/polls/')
+        element = driver.find_element_by_css_selector('body > h4')
+        self.assertEquals(element.text, 'List questions')
+        driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
